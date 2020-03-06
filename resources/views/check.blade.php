637fd3c6173652e582430244736115dbc89a8766
GabrielFemi <gabrielfemi799@gmail.com>
2020-03-06 17:18:24 +0100

    Fixed Token

--- resources/views/check.blade.php
+++ resources/views/check.blade.php
@@ -19,19 +19,20 @@
                         <div class="card-body p-4">
                             <div class="p-2">
                                 <h5 class="mb-5 text-center">View your Application Status</h5>
-                                <form class="form-horizontal" action="#">
+                                @include('messages')
+                            <form class="form-horizontal" action="{{ route('verify')}}" method="GET">
 
                                     <div class="row">
                                         <div class="col-md-12">
                                             
                                             <div class="form-group mb-4">
                                                 <label for="userpassword"><i class="mdi mdi-lock"></i> Application Token</label>
-                                                <input maxlength = "10" class="form-control" id="userpassword" placeholder="Enter Token">
+                                                <input name = "token" maxlength = "10" class="form-control" id="userpassword" placeholder="Enter Token">
                                             </div>
 
                                             
                                             <div class="mt-4">
-                                                <button class="btn btn-success btn-block waves-effect waves-light" type="submit">Log In</button>
+                                                <button class="btn btn-success btn-block waves-effect waves-light" type="submit">Check Appointment</button>
                                             </div>
                                             <div class="mt-4 text-center">
                                                 <a href="#" class="text-muted"><i class="mdi mdi-calendar-blank-multiple "></i> Book an Appointment?</a>
--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,5 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/about', function() {
-    return view('about');
-});
-
-
-Route::get('/contact', function() {
-    return view('contact');
-});
-Route::get('/apply', 'Applicants@validateApplicants');
-
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-*/
-Route::get('/', function(){
-    return view('index');
-});
-Route::get('/check', function (){
-    return view('check  ');
-});
+Route::get('/check', 'ApplicationsController@check')->name('check');
+Route::get('/verify', 'ApplicationsController@check_token')->name('verify');
+Route::get('/','ApplicationsController@index');
+?>
