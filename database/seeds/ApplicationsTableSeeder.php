df592d53e51e88e20c0eba4d238191ac8862e8e0
GabrielFemi <gabrielfemi799@gmail.com>
2020-03-16 16:14:31 +0100

    Created Database seeder for applications

---
+++ database/seeds/ApplicationsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+
+class ApplicationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('applications')->insert([
+            'appointment_date' => "2020-02-29",
+            'personal_message' => "I am broke and I need help.",
+            'application_token' => Str::random(10),
+            'created_at' => '2020-03-16 15:45:09',
+            'updated_at' => '2020-03-16 15:45:09'
+        ]);
+        
+    }
+}
