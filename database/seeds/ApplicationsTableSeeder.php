b6300a4b906e6dfc50ed1ac23be73e7db97fa175
GabrielFemi <gabrielfemi799@gmail.com>
2020-03-17 17:38:44 +0100

    Changed seeded token to 8 chars

--- database/seeds/ApplicationsTableSeeder.php
+++ database/seeds/ApplicationsTableSeeder.php
@@ -16,7 +16,7 @@ class ApplicationsTableSeeder extends Seeder
         DB::table('applications')->insert([
             'appointment_date' => "2020-02-29",
             'personal_message' => "I am broke and I need help.",
-            'application_token' => Str::random(10),
+            'application_token' => Str::random(8),
             'created_at' => '2020-03-16 15:45:09',
             'updated_at' => '2020-03-16 15:45:09'
         ]);
