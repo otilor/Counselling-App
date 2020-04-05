55918dc585e297d82d4237a57f3e8a22df85a356
GabrielFemi <gabrielfemi799@gmail.com>
2020-04-05 15:09:42 +0100

    created middleware for two types of admin - super admin and normal admin

---
+++ app/Http/Middleware/Admin/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware\Admin;
+
+use Closure;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
---
+++ app/Http/Middleware/Super_Admin/Super_Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware\Super_Admin;
+
+use Closure;
+
+class Super_Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        return $next($request);
+    }
+}
