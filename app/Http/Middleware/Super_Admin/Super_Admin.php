768503f045bb56e6add4338c26f6cdc160430a15
GabrielFemi <gabrielfemi799@gmail.com>
2020-04-06 09:19:42 +0100

    specified various routes for users

--- app/Http/Middleware/Super_Admin/Super_Admin.php
+++ app/Http/Middleware/Super_Admin/Super_Admin.php
@@ -17,15 +17,28 @@ class Super_Admin
      */
     public function handle($request, Closure $next)
     {
+        // This checks if the user is authenticated
         if (!Auth::check())
         {
             return redirect()->route('login');
         }
+        
+
+        // This checks if the user is a super admin
+
+        /**
+         * Role_id
+         * 0 is for super admin
+         * 1 is for normal admin
+         */
+        
         if (Auth::user()->role_id == 0)
         {
             return $next($request);
         }
 
+
+        // This evaluates if the above condition is false
         return redirect()->route('admin');
         
     }
